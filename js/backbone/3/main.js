ebf50414a7b24cb72083e5fe914a6a4e7db0ead3
Ruslan Karachun <free.changer@gmail.com>
2015-06-01 18:04:40 +0300

    Display JSON in marionette

--- js/backbone/3/main.js
+++ js/backbone/3/main.js
@@ -1,10 +1,24 @@
+function displayObject (object) {
+  if( typeof object === 'object')
+}
+
+
 var SingleLink = Marionette.ItemView.extend({
-  tagName: "li",
-  template: _.template("<a href='<%-path%>'><%-path%></a>")
+  tagName: "div",
+  //template: _.template("<a href='<%-path%>'><%-path%></a>")
+  //template: _.template(model)
+  template : function (model) {
+    console.log(model);
+    var output_string = '';
+    if(typeof model === 'object') {
+      output_string += '<span>[+]</span>';
+    }
+    return output_string + JSON.stringify(model);
+  }
 });
 
 var ListView = Marionette.CollectionView.extend({
-  tagName: 'ul',
+  tagName: 'div',
   childView: SingleLink
 });
 
