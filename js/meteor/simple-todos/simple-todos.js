64ee4b96e8570ba8443266528597fbdb0293413d
Ruslan Karachun <free.changer@gmail.com>
2015-07-02 22:25:18 +0300

    Final changes

--- js/meteor/simple-todos/simple-todos.js
+++ js/meteor/simple-todos/simple-todos.js
@@ -7,12 +7,18 @@ if (Meteor.isClient) {
       { text: "This is task 3", a: 31, b: 32, c: 33 },
     ],
 
+    items: [
+      { name: "Ruslan", number: 42 },
+      { name: "Ira", number: 911 },
+    ],
+
     view_item: function (item) {
       var objKeys = $.map(item, function (value, key) {
         return key;
       });
       var ret_val = $.map(objKeys, function (key) {
-        return JSON.stringify({"key": key, "val": item[key]});
+        //return JSON.stringify({"key": key, "val": item[key]});
+        return {"key": key, "val": item[key]};
       });
       console.log(ret_val);
       return ret_val;
