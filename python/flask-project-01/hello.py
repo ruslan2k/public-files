67fb7d7435af972a47c4a00f3e4a39c1288185b4
Ruslan Karachun <free.changer@gmail.com>
2015-09-10 18:11:58 +0300

    Add auth func

--- python/flask-project-01/hello.py
+++ python/flask-project-01/hello.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python
 
-from flask import Flask
+
+from functools import wraps
+from flask import Flask, request, Response
+
+
+def check_auth(username, password):
+    """This function is called to check if a username /
+    password combination is valid.
+    """
+    return username == 'admin' and password == 'secret'
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "Hello world"
+    return "Hello world<br>" \
+        + "<a href='http://flask.pocoo.org/snippets/8/'>doc</a>"
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
