3caa4ef68daede92220045166784fe70b25cb1de
Ruslan Karachun <free.changer@gmail.com>
2015-09-14 00:11:08 +0300

    Write readlint functions

--- erlang/words/words.erl
+++ erlang/words/words.erl
@@ -3,14 +3,18 @@
 -export([init/0]).
 -export([start/0]).
 
+
 start() ->
     register(words, spawn(?MODULE, init, [])).
+
 
 init() ->
     Words = get_words(),
     loop(Words).
 
+
 get_words() -> ["a", "b", "c"].
+
 
 loop(Words) ->
     receive
@@ -18,5 +22,25 @@ loop(Words) ->
             io:format("Received:~p~n", [Any]),
             loop(Words)
     end.
+
+
+readlines(FileName) ->
+    {ok, Device} = file:open(FileName, [read]),
+    try get_all_lines(Device)
+        after file:close(Device)
+    end.
+
+
+readlines_v2(FileName) ->
+    {ok, Data} = file:read_file(FileName),
+    binary:split(Data, [<<"\n">>], [global]).
+
+
+get_all_lines(Device) ->
+    case io:get_line(Device, "") of
+        eof -> [];
+        Line -> Line ++ get_all_lines(Device)
+    end.
+
 
 
