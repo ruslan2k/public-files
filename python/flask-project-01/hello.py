b143cf2d097240b17e5f3e580a8b39e8ff6bc469
Ruslan Karachun <free.changer@gmail.com>
2015-09-13 11:06:41 +0300

    Add PyMongo and app config

--- python/flask-project-01/hello.py
+++ python/flask-project-01/hello.py
@@ -3,7 +3,7 @@
 
 from functools import wraps
 from flask import Flask, request, Response
-
+from flask.ext.pymongo import PyMongo
 
 def check_auth(username, password):
     """This function is called to check if a username /
@@ -20,13 +20,23 @@ def authenticate():
 def require_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-        # FIXME
+        auth = request.authorization
+        if not auth \
+            or not check_auth(auth.username, auth.password):
+                return authenticate()
+        return f(*args, **kwargs)
+    return decorated
 
 
 app = Flask(__name__)
+app.config.from_pyfile('app.cfg')
+mongo = PyMongo(app)
 
 @app.route("/")
+#@require_auth
 def hello_world():
+    users = mongo.db.users.find({'online': True})
+    print users
     return "Hello world<br>" \
         + "<a href='http://flask.pocoo.org/snippets/8/'>doc</a>"
     
