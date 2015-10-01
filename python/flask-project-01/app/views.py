2fe94d32db68f6de78f6d905b3ba29dbc14635d4
Ruslan Karachun <free.changer@gmail.com>
2015-10-01 23:02:09 +0300

    try parse request

--- python/flask-project-01/app/views.py
+++ python/flask-project-01/app/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from app import app
 from forms import LoginForm
+from flask import request
 
 
 @app.route('/')
@@ -8,6 +9,7 @@ from forms import LoginForm
 def index():
     user = {'nickname': 'Ruslan'}    # user
     groups = ['Unix', 'Windows', 'QA']
+    uri = request.uri
     return render_template("index.html", title = 'Home', user = user, groups = groups)
 
 
