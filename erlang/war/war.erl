15df00e670aa52551161919b5bbdc06facbfbe54
Ruslan Karachun <free.changer@gmail.com>
2015-10-07 22:52:45 +0300

    Add template

---
+++ erlang/war/war.erl
@@ -0,0 +1,23 @@
+-module(war).
+-compile(export_all).
+
+start() ->
+    spawn(fun() -> loop([]) end).
+
+rpc(Pid, Request) ->
+    Pid ! {self(), Request},
+    receive
+        {Pid, Response} ->
+            Response
+    end.
+
+loop(X) ->
+    receive
+        Any ->
+            io:format("Received:~p~n", [Any]),
+            loop(X)
+    end.
+
+
+
+
