42d57206d44528d024a17a75f36610b4be50d392
Ruslan Karachun <free.changer@gmail.com>
2015-11-04 18:58:51 +0300

    Refactor

--- erlang/Cesarini-2009/11/store.erl
+++ erlang/Cesarini-2009/11/store.erl
@@ -10,15 +10,24 @@ start_master() ->
 %%  start_slave(Master) ->
 %%     spawn(
 
-server(NodesList) ->
+server(Nodes) ->
     receive
         {From, slave_connect, Name} ->
+            io:format("Nodes: ~p~n", [Nodes]),
             io:format("From <~p>, Name <~p>~n", [From, Name]),
-            New_NodesList = slave_connect(From, Name)
+            New_Nodes = slave_connect(From, Name, Nodes),
+            server(New_Nodes)
     end.
 
-slave_connect(From, Name) ->
-    [].
+slave_connect(From, Name, Nodes) ->
+    case lists:keymember(Name, 2, Nodes) of
+        true ->
+            From ! {master, stop, node_exists},
+            Nodes;
+        false ->
+            From ! {master, logged_on},
+            [{From, Name} | Nodes]
+    end.
+
     
-
-
+% EOF
