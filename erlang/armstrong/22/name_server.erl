d7fb77a0e83340795029e2d69e42e46b8ef10cc1
Ruslan Karachun <free.changer@gmail.com>
2015-11-30 21:20:19 +0300

    Add functions

--- erlang/armstrong/22/name_server.erl
+++ erlang/armstrong/22/name_server.erl
@@ -9,3 +9,5 @@ add(Name, Place) -> rpc(name_server, {add, Name, Place}).
 find(Name)       -> rpc(name_server, {find, Name}).
 
 init() -> dict:new().
+handle({add, Name, Place}, Dict) -> {ok, dict:store(Name, Place, Dict)};
+handle({find, Name}, Dict)       -> {dict:find(Name, Dict), Dicr}.
