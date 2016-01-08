738b5024234abb21f7ec134469e9da3648a5a339
Ruslan Karachun <free.changer@gmail.com>
2016-01-08 19:15:55 +0300

    Add model handler

--- erlang/cowboy/a/hello_erlang/src/hello_erlang_app.erl
+++ erlang/cowboy/a/hello_erlang/src/hello_erlang_app.erl
@@ -6,12 +6,15 @@
 
 start(_Type, _Args) ->
     Dispatch = cowboy_router:compile([
-        {'_', [{"/", hello_handler, []}]}
+        {'_', [
+            {"/", hello_handler, []},
+            {"/:model", model_handler, []}
+        ]}
     ]),
     cowboy:start_http(my_http_listener, 100, [{port, 8080}],
         [{env, [{dispatch, Dispatch}]}]
     ),
-	hello_erlang_sup:start_link().
+    hello_erlang_sup:start_link().
 
 stop(_State) ->
 	ok.
