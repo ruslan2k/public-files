856063f0c459654295a20fa7b4d8c0f6cedaaa99
Ruslan Karachun <free.changer@gmail.com>
2016-01-18 21:24:12 +0300

    Add sup

---
+++ erlang/21/ch_sup.erl
@@ -0,0 +1,20 @@
+%% http://www.erlang.org/doc/design_principles/sup_princ.html
+
+-module(ch_sup).
+-behavior(supervisor).
+
+-export([start_link/0]).
+-export([init/1]).
+
+start_link() ->
+    supervisor:start_link(ch_sup, []).
+
+init(_Args) ->
+    SupFlags = #{strategy => one_for_one, intensity => 1, period =>5},
+    ChildSpecs = [#{id => ch3,
+                    start => {ch3, start_link, []},
+                    restart => permanent,
+                    shutdown => brutall_kill,
+                    type => worker,
+                    modules =>[cg3]}],
+    {ok, {SupFlags, ChildSpecs}}.
