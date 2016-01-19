a6e29f5306bea0258befb8c8e76bcdaaafecc259
Ruslan Karachun <free.changer@gmail.com>
2016-01-19 10:59:22 +0300

    Add ch3 module

--- erlang/21/ch3.erl
+++ erlang/21/ch3.erl
@@ -1,12 +1,13 @@
 -module(ch3).
 -behaviour(gen_server).
 
+-export([start_link/0]).
+
 -export([alloc/1]).
 -export([free/1]).
 
--export([start_link/0]).
 -export([init/1]).
--export([]).
--export([]).
--export([]).
-
+-export([handle_call/3]).
+-export([handle_cast/2]).
+-export([handle_info/2]).
+-export([terminate/2]).
--- erlang/21/ch_app.app
+++ erlang/21/ch_app.app
@@ -1,8 +1,8 @@
 {application, ch_app,
  [{description, "Channel allocator"},
   {vsn, "1"},
-  {modules, {ch_app, ch_sup, ch3}},
+  {modules, [ch_app, ch_sup, ch3]},
   {registered, [ch3]},
-  {applications, [kernel, stdlib. sasl]},
+  {applications, [kernel, stdlib, sasl]},
   {mod, {ch_app, []}}
 ]}.
