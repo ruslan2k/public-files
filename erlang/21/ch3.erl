8a78dcdbfb282752eab22508fd106f544522429e
Ruslan Karachun <free.changer@gmail.com>
2016-01-26 18:11:30 +0300

    Add ch3 module

--- erlang/21/ch3.erl
+++ erlang/21/ch3.erl
@@ -11,3 +11,35 @@
 -export([handle_cast/2]).
 -export([handle_info/2]).
 -export([terminate/2]).
+
+start_link() ->
+    gen_server:start_link({local, ch3}, ch3, [], []).
+
+init(_Args) ->
+    {ok, #{channels => []}}.
+
+alloc(Channel) ->
+    gen_server:call(ch3, {alloc. Chanel}).
+
+free(Ch) ->
+    gen_server:cast(ch3, {free, Ch}).
+
+handle_call({alloc, Ch}, _From, #{channels := Chs} = State) ->
+    Chs_new = [Ch | Chs],
+    {reply, ok, State#{channels => Chs_new}}.
+
+handle_cast({free, Ch}, #{channels := Chs} = State) ->
+    Chs2 = lists:filter(fun(X) ->
+                            if X == Ch -> false;
+                                true -> true
+                            end
+                        end, Chs),
+    {noreply, State#{channels => Chs2}}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVersion, Library, _Extra) -> {ok, Library}.
--- erlang/21/ch_sup.erl
+++ erlang/21/ch_sup.erl
@@ -10,7 +10,7 @@ start_link() ->
     supervisor:start_link(ch_sup, []).
 
 init(_Args) ->
-    SupFlags = #{strategy => one_for_one, intensity => 1, period =>5},
+    SupFlags = #{strategy => one_for_one, intensity => 2, period =>5},
     ChildSpecs = [#{id => ch3,
                     start => {ch3, start_link, []},
                     restart => permanent,
