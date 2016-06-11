1acbf78362dd6e264a1976e24f590a53b6936669
Ruslan Karachun <free.changer@gmail.com>
2016-06-12 02:00:36 +0300

    Add test with catch

---
+++ c/00-hello-world/test.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+unsigned int squarings (unsigned int x)
+{
+    return x * x;
+}
+
+TEST_CASE("Squaring are computed", "[squarings]") {
+    REQUIRE( squarings(1) == 1);
+}
