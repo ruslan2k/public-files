eeafaa8c20cecda70018c64e7bcd1264be5606fb
Ruslan Karachun <free.changer@gmail.com>
2016-06-16 19:19:30 +0300

    Read key codes

--- c/00-hello-world/hello-2.c
+++ c/00-hello-world/hello-2.c
@@ -1,20 +1,45 @@
+// #include <time.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <ncurses.h>
+
+int getDirection ();
+int * movePoint (int, int);
 
 int main ()
 {
+    int i = 150;
     int ch;
-    int row, col;
+    int *p_coord;
+    int row;
+    int col;
+
     initscr();
+    getmaxyx(stdscr, row, col);
+    // row = row / 2;
+    // col = col / 2;
+    printw("pres any key");
+
+    row = col = 0;
 
     do {
-        getmaxyx(stdscr, row, col);
-        printw("Hello, world! row: %d, col: %d\n", row, col);
+        sleep(1);
+        //usleep(1000);
+        p_coord = movePoint(col, row);
+        // col = p_coord[0];
+        // row = p_coord[1];
         ch = getch();
-        printw("[ %d ]", ch);
+        //sleep(1);
+        mvprintw(row, col, "# i:%d r:%d c:%d ", i, row, col);
+        printw("[key code: %d]", ch);
         refresh();
+        col++;
+        row++;
 
-    } while (ch != 27);
+    } while (i--);
+    //} while (ch != 27);
+
+    getch();
     endwin();
 
     return 0;
@@ -22,6 +47,27 @@ int main ()
 
 int getDirection ()
 {
-    return rand();
+    return rand() % 4;
 }
 
+int * movePoint (int x, int y)
+{
+    switch (getDirection()) {
+        case 0:
+            y--;
+            break;
+        case 1:
+            y++;
+            break;
+        case 2:
+            x--;
+            break;
+        case 3:
+            x++;
+            break;
+    }
+    static int r[2];
+    r[0] = x;
+    r[1] = y;
+    return r;
+}
