91834da29741bdcd6d2a3bc1ccafd9e5a876a1be
Ruslan Karachun <free.changer@gmail.com>
2016-06-19 01:11:15 +0300

    Add moveing player

--- c/00-hello-world/hello-2.c
+++ c/00-hello-world/hello-2.c
@@ -3,6 +3,11 @@
 #include <unistd.h>
 #include <ncurses.h>
 
+#define DEF_UP 1
+#define DEF_DOWN 2
+#define DEF_RIGHT 3
+#define DEF_LEFT 4
+
 int getDirection ();
 int * movePoint (int, int);
 
@@ -13,28 +18,30 @@ int main ()
     int *p_coord;
     int row;
     int col;
+    int player_x;
+    int player_y;
 
     initscr();
     getmaxyx(stdscr, row, col);
-    // row = row / 2;
-    // col = col / 2;
-    printw("pres any key");
-
-    row = col = 0;
+    row = row / 2;
+    col = col / 2;
+    mvprintw(0, col, "pres cursor key");
+    mvprintw(row, col, "@");
+    noecho();
 
     do {
-        sleep(1);
-        //usleep(1000);
-        p_coord = movePoint(col, row);
-        // col = p_coord[0];
-        // row = p_coord[1];
-        ch = getch();
         //sleep(1);
-        mvprintw(row, col, "# i:%d r:%d c:%d ", i, row, col);
-        printw("[key code: %d]", ch);
+        //usleep(1000);
+
+        int * p_new_coord = movePoint(col, row);
+
+        mvprintw(row, col, " ");
+        col = p_new_coord[0];
+        row = p_new_coord[1];
+        mvprintw(0, 0, "r:%d c:%d  ", row, col);
+        mvprintw(row, col, "@");
+
         refresh();
-        col++;
-        row++;
 
     } while (i--);
     //} while (ch != 27);
@@ -45,24 +52,53 @@ int main ()
     return 0;
 }
 
-int getDirection ()
+int getDirection0 ()
 {
     return rand() % 4;
+}
+
+int getDirection ()
+{
+    if (getch() != 27) {
+        return 0;
+    }
+    if (getch() != 91) {
+        return 0;
+    }
+    int ret_val;
+    switch (getch()) {
+        case 65:
+            ret_val = DEF_UP;
+            break;
+        case 66:
+            ret_val = DEF_DOWN;
+            break;
+        case 67:
+            ret_val = DEF_RIGHT;
+            break;
+        case 68:
+            ret_val = DEF_LEFT;
+            break;
+        default:
+            ret_val = 0;
+            break;
+    }
+    return ret_val;
 }
 
 int * movePoint (int x, int y)
 {
     switch (getDirection()) {
-        case 0:
+        case DEF_UP:
             y--;
             break;
-        case 1:
+        case DEF_DOWN:
             y++;
             break;
-        case 2:
+        case DEF_LEFT:
             x--;
             break;
-        case 3:
+        case DEF_RIGHT:
             x++;
             break;
     }
