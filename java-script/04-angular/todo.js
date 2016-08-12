f212ee2ccbda976fcaf924a65f0974cc53f14b86
Ruslan Karachun <free.changer@gmail.com>
2016-08-12 14:10:06 +0300

    Added api

--- java-script/04-angular/todo.js
+++ java-script/04-angular/todo.js
@@ -1,24 +1,40 @@
 angular.module('todoApp', [])
-  .controller('todoListCtrl', TodoListCtrl);
+  .controller('todoListCtrl', TodoListCtrl)
+  .factory('todoAppApi', todoAppApi);
 
-function TodoListCtrl ($scope) {
-  $scope.todos = [
-    {text: 'bla'},
-    {text: 'abc'},
-  ];
+function TodoListCtrl ($scope, todoAppApi) {
+  $scope.todos = todoAppApi.getTodos();
+  //$scope.todos = [];
+  //$scope.isLoading = isloading;
+  //$scope.refreshTodos = refreshTodos;
+
+  //var loading = false;
+
+  //function isloading () {
+  //  return loading;
+  //}
+
+  //function refreshTodos () {
+  //  loading = true;
+  //  $scope.todos = [];
+  //  setTimeout( function () {
+  //    $scope.todos = todoAppApi.getTodos();
+  //    loading = false;
+  //    $scope.$apply();
+  //  }, 2000);
+  //}
 }
 
-
-
-angular.module("diveLog", [])
-  .controller('diveLogCtrl', DiveLogCtrl);
-
-
-function DiveLogCtrl ($scope) {
-  $scope.totalDives = 4;
-  $scope.dives = [
-    {text: "text 1"},
-    {text: "text 2"},
-    {text: "text 3"},
+function todoAppApi () {
+  var todos = [
+    {text: "abc"},
+    {text: "def"},
+    {text: "ghi"},
   ];
+
+  return {
+    getTodos: function () {
+      return todos;
+    }
+  };
 }
