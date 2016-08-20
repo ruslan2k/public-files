0eeef0e3c732bf45caaa113f686b498de2cd0d22
Ruslan Karachun <free.changer@gmail.com>
2016-08-20 19:16:48 +0300

    Added static folders

--- java-script/04-angular/express.js
+++ java-script/04-angular/express.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var port = process.env.PORT || 8008;
 
-app.git('/', function (req, res) {
+//app.use(express.static('js'));
+['/js', '/node_modules'].forEach(function (dir) {
+  app.use(dir, express.static(__dirname + dir));
+});
+
+app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-all.listen(8008);
+
+console.log('http://localhost:'+ port);
+
+app.listen(port);
