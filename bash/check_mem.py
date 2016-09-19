4c02e82eec6c833d8455be53d6f828acfa438e54
Ruslan Karachun <free.changer@gmail.com>
2016-09-19 15:49:01 +0000

    Fix output

--- bash/check_mem.py
+++ bash/check_mem.py
@@ -16,13 +16,13 @@ for pid in psutil.pids():
     else:
         user_dict[username] = {}
         user_dict[username]["rss_sum"] = p.memory_info().rss
-pp.pprint(user_dict)
+#pp.pprint(user_dict)
 
 rss_dict = {mem["rss_sum"]:user for user,mem in list(user_dict.items())}
 max_rss = ({rss:rss_dict[rss] for rss in sorted(rss_dict,reverse=True)[0:DEF_ELEM]})
 
-pp.pprint(rss_dict)
-pp.pprint(max_rss)
+#pp.pprint(rss_dict)
+#pp.pprint(max_rss)
 mem_str = "OK - all rss {0} |all_users={0}".format(all_mem)
 sss = ";;; ".join("{}={}".format(u, m) for m, u in max_rss.items())
 print("{0};;; {1}".format(mem_str, sss))
