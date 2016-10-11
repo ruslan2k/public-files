39b89b8e1e108994546f9ebef9ce9e44d6f892b0
Ruslan Karachun <free.changer@gmail.com>
2016-10-11 16:30:10 +0000

    Added registeration form

--- python/04-django/polls/views.py
+++ python/04-django/polls/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
 from django.contrib.auth.models import Group
 
-#from .models import SecurityGroup
+import pprint as pp
 
 
 def index(request):
     all_groups = Group.objects.all()
     output = ', '.join([g.name for g in all_groups])
     return HttpResponse(output)
+
+def register(request):
+    pp.pprint(request)
+    form = """<form method="post">
+    <input type="text" name="login">
+    <input type="submit" value="login">
+    </form>
+    """
+    return HttpResponse(form)
