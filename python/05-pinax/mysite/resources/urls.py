643098700a1bbfac7ebc161497b1a71fee57ff14
Ruslan Karachun <free.changer@gmail.com>
2016-10-20 12:23:41 -0400

    Added items and delete link

--- python/05-pinax/mysite/resources/urls.py
+++ python/05-pinax/mysite/resources/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<resource_id>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^item/(?P<item_id>[0-9]+)/$', views.delete_item, name='delete_item'),
 ]
--- python/05-pinax/mysite/resources/views.py
+++ python/05-pinax/mysite/resources/views.py
@@ -5,26 +5,25 @@ from django.contrib.auth.models import Group
 
 from django.contrib.auth.decorators import login_required
 
-from resources.models import Resource
+from resources.models import Resource, Item
 
 import pprint as pp
 
-from .forms import ResourceForm
+from .forms import ResourceForm, ItemForm
 #from django.contrib.auth.models import Group
 
 @login_required(login_url='/accounts/login/')
 def index(request):
-    resources = Resource.objects.all()
+    pp.pprint(request.user)
+    resources = request.user.resource_set.all()
+    #resources = Resource.objects.all()
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = ResourceForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
             pp.pprint(form.cleaned_data)
-            resource = Resource( \
-                    name=form.cleaned_data["resource_name"],
-                    user_id=0
-            )
+            resource = Resource(name=form.cleaned_data["resource_name"], user_id=request.user.id)
             resource.save()
             return HttpResponseRedirect('/resources')
     else:
@@ -33,9 +32,21 @@ def index(request):
     return render(request, "resources/index.html", context)
 
 
+@login_required(login_url='/accounts/login/')
 def detail(request, resource_id):
     resource = get_object_or_404(Resource, pk=resource_id)
-    context = {"resource": resource}
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            item = Item(key=form.cleaned_data["item_key"], val=form.cleaned_data["item_val"],
+                    resource_id=resource_id)
+            item.save()
+            return HttpResponseRedirect("/resources/%s/" % resource_id)
+    else:
+        form = ItemForm()
+    context = {"resource": resource, "form": form}
     return render(request, "resources/detail.html", context)
 
 
+def delete_item(request, item_id):
+    pass
