adb116703eb985a3eb8fdf3af7b7070c6204918b
Ruslan Karachun <free.changer@gmail.com>
2016-10-24 11:21:31 +0000

    Added ram info

--- bash/check_mem.py
+++ bash/check_mem.py
@@ -9,11 +9,11 @@ import pprint as pp
 
 DEF_ELEM = 3
 user_dict = {}
-all_mem = 0
+all_rss = 0
 
 for pid in psutil.pids():
     p = psutil.Process(pid)
-    all_mem += p.memory_info().rss
+    all_rss += p.memory_info().rss
     username = p.username()
     if username in user_dict:
         user_dict[username]["rss_sum"] += p.memory_info().rss
@@ -25,8 +25,7 @@ for pid in psutil.pids():
 rss_dict = {mem["rss_sum"]:user for user,mem in list(user_dict.items())}
 max_rss = ({rss:rss_dict[rss] for rss in sorted(rss_dict,reverse=True)[0:DEF_ELEM]})
 
-#pp.pprint(rss_dict)
-#pp.pprint(max_rss)
-mem_str = "OK - all rss {0} |all_users={0}".format(all_mem)
+v_mem = psutil.virtual_memory()
+mem_str = "OK - all rss {0} |ram={1};;; all_users={0}".format(all_rss, v_mem.total)
 sss = ";;; ".join("{}={}".format(u, m) for m, u in max_rss.items())
 print("{0};;; {1}".format(mem_str, sss))
