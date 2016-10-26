12e517fc9d83cdcebeb63f7118561ffc50ba2bdb
Ruslan Karachun <free.changer@gmail.com>
2016-10-26 10:58:43 -0400

    Added test

--- python/05-pinax/mysite/resources/tests.py
+++ python/05-pinax/mysite/resources/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+import pprint as pp
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class FirsTest(TestCase):
+    def test_test_test(self):
+        self.assertEqual(404, 404)
+
+class UserTest(TestCase):
+    def setUp(self):
+        user = User.objects.create_user('test', 'test@email.com', 'test')
+        self.client = Client()
+
+    def test_login(self):
+        logged_in = self.client.login(username='test', password='test')
+        self.assertEqual(logged_in, True)
+        #pp.pprint(logged_in)
+
+
+
