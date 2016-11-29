9394e7113e3de0914e6ab011991209115e2d24f4
Ruslan Karachun <free.changer@gmail.com>
2016-11-29 14:30:02 +0000

    Added tests

--- python/05-pinax/mysite/resources/tests.py
+++ python/05-pinax/mysite/resources/tests.py
@@ -2,6 +2,33 @@ import pprint as pp
 from django.test import TestCase, Client
 from django.contrib.auth.models import User
 
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium.webdriver.firefox.webdriver import WebDriver
+
+
+class MySeleniumTests(StaticLiveServerTestCase):
+    fixtures = ['user-data.json']
+
+    @classmethod
+    def setUpClass(cls):
+        super(MySeleniumTests, cls).setUpClass()
+        cls.selenium = WebDriver()
+        cls.selenium.implicitly_wait(10)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.selenium.quit()
+        super(MySeleniumTests, cls).tearDownClass()
+
+    def test_login(self):
+        self.selenium.get('%s%s' % (self.live_server_url, '/login/'))
+        username_input = self.selenium.find_element_by_name("username")
+        username_input.send_keys('myuser')
+        password_input = self.selenium.find_element_by_name("password")
+        password_input.send_keys('secret')
+        self.selenium.find_element_by_xpath('//input[@value="Login"]').click()
+
+
 class FirsTest(TestCase):
     def test_test_test(self):
         self.assertEqual(404, 404)
@@ -15,6 +42,3 @@ class UserTest(TestCase):
         logged_in = self.client.login(username='test', password='test')
         self.assertEqual(logged_in, True)
         #pp.pprint(logged_in)
-
-
-
