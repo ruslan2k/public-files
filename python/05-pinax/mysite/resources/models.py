a1d9a9d546ef94584ff872acc5f905c4c53b5104
Ruslan Karachun <free.changer@gmail.com>
2016-11-29 16:11:20 +0000

    Added django tests

--- python/05-pinax/mysite/resources/models.py
+++ python/05-pinax/mysite/resources/models.py
@@ -32,6 +32,14 @@ class Item(models.Model):
         return self.key
 
 
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    salt = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.user.name
+
+
 class SmartUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     salt = models.CharField(max_length=255)
@@ -57,18 +65,3 @@ def model_pre_save(sender, **kwargs):
     print('Saving: {}'.format(kwargs['instance'].__dict__))
 
 
-#class Question(models.Model):
-#    question_text = models.CharField(max_length=200)
-#    pub_date = models.DateTimeField('date published')
-#
-#class Choice(models.Model):
-#    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-#    choice_text = models.CharField(max_length=200)
-#    votes = models.IntegerField(default=0)
-#
-#class SecurityGroup(models.Model):
-#    group = models.OneToOneField(Group, on_delete=models.CASCADE)
-#    custom_field = models.CharField(max_length=191)
-#
-#    def __str__(self):
-#        return self.group.name
