fab4e75d00a84138fce326b99aa38f7c9a0b5cd1
Ruslan Karachun <free.changer@gmail.com>
2016-12-06 16:16:31 +0000

    Fix

--- python/05-pinax/mysite/tests/casper.js
+++ python/05-pinax/mysite/tests/casper.js
@@ -2,30 +2,23 @@ var casper = require('casper').create();
 
 casper.start('http://ru0.aptinfo.net:8000/account/signup/', function () {
   this.echo(this.getTitle());
+  this.waitForSelector('form[action="/account/signup/"]');
 });
 
-casper.waitForSelector("form", function () {
-  this.fill('form#singup_form', {
-    'input[name=username]': 'u1',
-    'input[name=password]': 'u1',
-    'input[name=password_confirm]': 'u1',
-    'input[name=email]': 'u1@aptinfo.net',
+casper.then(function () {
+  this.fill('form#signup_form', {
+    username: 'u1',
+    password: 'u1',
+    password_confirm: 'u1',
+    email: 'u1@aptinfo.net',
   }, true);
 });
 
 
-// casper.open('http://localhost:8000/account/signup/', {
-//   method: 'post',
-//   date: {
-//     username: 'u1',
-//     password: 'p1',
-//     password_confirm: 'p1',
-//     email: 'u1@aptinfo.net',
-//   }
-// });
+casper.then(function () {
+  this.echo('ok');
+  casper.capture('1.png');
+});
 
-// casper.thenOpen('http://phantomjs.org', function() {
-//     this.echo(this.getTitle());
-// });
 
 casper.run();
