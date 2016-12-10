e13389068b0848fb0fb3f0e2ccaa351f746bea13
Ruslan Karachun <free.changer@gmail.com>
2016-12-11 00:49:18 +0300

    Tested with User tbl

--- python/06-django/1.py
+++ python/06-django/1.py
@@ -4,8 +4,14 @@ import os
 os.environ["DJANGO_SETTINGS_MODULE"]="mysite.settings"
 django.setup()
 
+from django.contrib.auth.models import User
 from test2.models import Profile
 
-p = Profile(random_salt='123')
-p.save()
+print(Profile.objects.all())
 
+u1 = User(username='u1', email='u1@examle.com')
+u1.save()
+
+
+p = Profile(random_salt='123', user=u1)
+p.save()
--- python/06-django/test2/models.py
+++ python/06-django/test2/models.py
@@ -11,8 +11,6 @@ class Profile(models.Model):
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     print('create_user_profile')
+    print(instance)
     if created:
         Profile.objects.create(user=instance)
-
-
-
