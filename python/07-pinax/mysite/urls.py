5ce453b55d8a9b3223dd2d93be190d90a2d2d294
Ruslan Karachun <free.changer@gmail.com>
2016-12-12 16:22:53 +0000

    Added SignupView class

--- python/07-pinax/mysite/urls.py
+++ python/07-pinax/mysite/urls.py
@@ -5,6 +5,8 @@ from django.views.generic import TemplateView
 
 from django.contrib import admin
 
+from resources.views import SignupView
+
 
 urlpatterns = [
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
--- python/07-pinax/resources/views.py
+++ python/07-pinax/resources/views.py
@@ -1,3 +1,13 @@
+import account.views
+
 from django.shortcuts import render
 
-# Create your views here.
+# TODO
+# http://blog.pinaxproject.com/2015/10/12/recap-september-pinax-hangout/
+# http://django-user-accounts.readthedocs.io/en/latest/usage.html
+
+
+class SignupView(account.views.SignupView):
+    pass
+
+
