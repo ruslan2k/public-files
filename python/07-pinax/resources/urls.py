0e1b4facf82cea594ee1926e31fdd9300a749f9c
Ruslan Karachun <free.changer@gmail.com>
2016-12-15 17:16:13 +0000

    Added urls

--- python/07-pinax/resources/urls.py
+++ python/07-pinax/resources/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    # ex: /resources/test/
+    # /resources/
+    url(r'^$', views.index, name='index').
+    # /resources/5/
+    url(r'^(?P<resource_id>[0-9]+)/$', views.detail, name='detail'),
+    # /resources/test/
     url(r'^test/$', views.test, name='test'),
 ]
 
