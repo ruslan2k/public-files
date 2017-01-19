3246cfcc3385f52b1a37707d7a64957819bb4975
Ruslan Karachun <free.changer@gmail.com>
2017-01-19 21:07:02 +0000

    Added logger

--- android/01-browser-app/app.js
+++ android/01-browser-app/app.js
@@ -1,5 +1,10 @@
 $(function() {
 
-  console.log("ready!");
+  $('body').append('<button id="log">L</logger>');
+  $('body').append('<div id="logger"></div>');
+  $('#logger').append('test');
+  $('#log').click(function () {
+    $('#logger').toggle();
+  });
 
 });
