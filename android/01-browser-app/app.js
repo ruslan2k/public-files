0eff515788f3f70e85fc60316889bf63b49feb21
Ruslan Karachun <free.changer@gmail.com>
2017-01-22 10:13:58 +0000

    Added logging with styles

--- android/01-browser-app/app.js
+++ android/01-browser-app/app.js
@@ -1,20 +1,44 @@
 $(function() {
 
-  $('body').append('<button id="log">L</logger>');
-  $('body').append('<div id="logger"></div>');
-  $('#logger').append('test');
-  $('#log').click(function () {
-    $('#logger').toggle();
-  });
+  function init (element) {
+    $(element).append('<div><button id="log">log</button></div>');
+    $('<div/>', {
+      id: 'logger',
+      css: {
+        border: '1px solid',
+        display: 'none',
+      }
+    }).appendTo(element);
 
-  console.olog = console.log;
-  console.log = function (message) {
-    console.olog(message);
-    $('#logger').append('<p>' + message + '</p');
+    $('#log').click(function () {
+      $('#logger').toggle();
+    });
+
+    console.olog = console.log;
+    console.log = function (message) {
+      console.olog(message);
+      $('#logger').append(message + '<br/>');
+    }
   }
+
+  init('#logging');
 
   $('#test').click(function () {
     console.log('test');
+    // $.ajax({
+    //   url: "https://wm.exchanger.ru/asp/XMLWMList.asp?exchtype=17",
+    // }).done(function () {
+    //   console.log('ajax - ok');
+    // });
   });
+ 
+  function timeout () {
+    setTimeout( function () {
+      console.log('tick');
+      timeout();
+    }, 10000);
+  }
+
+  timeout();
 
 });
