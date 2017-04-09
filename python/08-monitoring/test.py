b6686f5299770fda8ee0f06a88b0d08d2d43172c
Ruslan Karachun <free.changer@gmail.com>
2017-04-09 23:21:11 +0300

    Added libs

--- python/08-monitoring/test.py
+++ python/08-monitoring/test.py
@@ -1,12 +1,14 @@
-
+#!/usr/bin/env python
 
 import yaml
 import numpy as np
+import sqlite3
 import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
-f_name = "/home/ruslan/Downloads/a.png"
+
+f_name = "/tmp/a.png"
 
 t = np.arange(0., 10., 0.1)
 #plt.plot(t, t, 'r--', t, t**2, 'bs', t, t**3, 'g^')
