952f648a817d9bbfb10ec0be3069d83e53037981
Ruslan Karachun <free.changer@gmail.com>
2017-04-11 15:00:50 +0300

    Added monitoring commands

--- python/08-monitoring/test.py
+++ python/08-monitoring/test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+#import commands
 import yaml
 import numpy as np
 import sqlite3
@@ -7,6 +8,7 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
+from subprocess import Popen, PIPE
 
 f_name = "/tmp/a.png"
 
@@ -15,8 +17,26 @@ t = np.arange(0., 10., 0.1)
 plt.plot(t, t, 'r--', t, t**2, 'bs')
 plt.savefig(f_name)
 
+conn = sqlite3.connect('monitoring.sq3')
+c = conn.cursor()
+
 with open("config.yml", "r") as f:
     config = yaml.load(f)
 
 for metric in config:
-    print(metric)
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS {} (
+        id INTEGER PRIMARY KEY ASC, 
+        time timestamp default (strftime('%s', 'now')),
+        value text )""".format(metric['name']))
+    conn.commit()
+
+for metric in config:
+    print(metric['name'])
+    #print(metric)
+    p = Popen((metric['command']).split(), stdout=PIPE)
+    output = p.communicate()[0]
+    print(p.returncode)
+    print(output)
+
+
