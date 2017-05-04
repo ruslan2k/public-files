95b866c9c4bb75a9f2a059dbdf0e00b0df2335a6
Ruslan Karachun <free.changer@gmail.com>
2017-05-04 18:28:31 +0300

    Added web bot

---
+++ python/09-currency/app.py
@@ -0,0 +1,39 @@
+import os
+import datetime
+import json
+import urllib
+import urllib.parse
+import urllib.request
+import pprint as pp
+
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+pp.pprint(os.environ.get("server"))
+url = os.environ.get("url")
+
+print(datetime.date.today())
+
+today = datetime.date.today()
+month_ago = today - datetime.timedelta(days=30)
+
+print(month_ago.isoformat())
+
+def display(date):
+    global url
+    url_values = urllib.parse.urlencode({"onDate": date.isoformat(), "Periodicity": 0})
+    url = url.format(145, url_values)
+    pp.pprint(url)
+    response = urllib.request.urlopen(url)
+    string = response.read().decode("utf-8")
+    json_obj = json.loads(string)
+    pp.pprint(json_obj["Cur_OfficialRate"])
+    pp.pprint(json_obj)
+
+
+display(month_ago)
+display(today)
+
