ae12bca305cc06da1e7fbe7092fefe67bc2b5a3a
Ruslan Karachun <free.changer@gmail.com>
2017-07-20 11:23:28 +0300

    Added infinity loop

--- python/11-daemon/monitor.py
+++ python/11-daemon/monitor.py
@@ -7,16 +7,33 @@ from datetime import datetime
 
 
 def playload():
-    subprocess.Popen(config.DEF_PROG)
+    #subprocess.Popen([config.DEF_PROG])
+    subprocess.Popen(
+            [config.DEF_PROG],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            #creationflags=subprocess.CREATE_NEW_PROCESS_GROUP
+    )
+    print('return')
 
 
 def daemon():
     tmp_dir = os.getenv('TMP')
     log_file = os.path.join(tmp_dir, 'monitor.log');
-    for x in range(24):
-        with open(log_file, 'a+') as f:
-            f.write(datetime.now().isoformat() + "\n")
+    while True:
+        now = datetime.now()
+        if (now.hour < 10) or (now.hour > 18):
+            continue
+        if (now.isoweekday() > 5) or (now.isoweekday() == 0):
+            continue
+        print('playload')
+        playload()
+        print('sleep')
         time.sleep(3600)
+    #for x in range(24):
+    #    with open(log_file, 'a+') as f:
+    #        f.write(datetime.now().isoformat() + "\n")
+    #    time.sleep(3600)
 
 
 def main():
