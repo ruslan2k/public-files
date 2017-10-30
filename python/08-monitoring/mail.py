398b9dce1ac38140ec52dd16358c1f9ee738328c
Ruslan <free.changer@gmail.com>
2017-10-30 14:41:13 +0300

    Fixed mail

--- python/08-monitoring/mail.py
+++ python/08-monitoring/mail.py
@@ -4,10 +4,11 @@ from config import *
 
 msg = MIMEText('test')
 msg['Subject'] = '- test -'
+msg['From'] = fromaddr
 
 server = smtplib.SMTP_SSL(mail_srv, port)
 server.set_debuglevel(1)
 server.login(fromaddr, password)
-server.sendmail(fromaddr, toaddrs, 'test')
+server.sendmail(fromaddr, toaddrs, msg.as_string())
 server.quit()
 
