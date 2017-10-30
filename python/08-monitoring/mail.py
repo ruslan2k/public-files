af8d482b246a7cb2977240c59a74bff7c47a2ca9
Ruslan <free.changer@gmail.com>
2017-10-30 12:49:55 +0300

    Added subject

--- python/08-monitoring/mail.py
+++ python/08-monitoring/mail.py
@@ -1,6 +1,9 @@
 import smtplib
+from email.mime.text import MIMEText
 from config import *
 
+msg = MIMEText('test')
+msg['Subject'] = '- test -'
 
 server = smtplib.SMTP_SSL(mail_srv, port)
 server.set_debuglevel(1)
