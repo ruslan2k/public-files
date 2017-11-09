851495c19734261848c8a43299aafab30b08197e
Ruslan Karachun <free.changer@gmail.com>
2017-11-09 11:55:47 +0000

    Added path var

--- java-script/09-proxy/index.js
+++ java-script/09-proxy/index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.all('/*', (req, res) => {
   //return res.send('Hello World', req.query)
-  return res.json({'url': req.url})
+  return res.json({'url': req.url, 'path': req.path})
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
