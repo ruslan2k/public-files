425e41810bab92491afe1878ac696b124a7d8a97
Ruslan <free.changer@gmail.com>
2017-11-10 13:43:40 +0300

    Added request

--- java-script/09-proxy/index.js
+++ java-script/09-proxy/index.js
@@ -1,13 +1,22 @@
 const express = require('express')
+const request = require('request')
 const app = express()
 
-app.all('/*', (req, res) => {
-  //return res.send('Hello World', req.query)
-  return res.json({'url': req.url, 'path': req.path})
+console.log('Promise', typeof Promise);
+
+app.get('/*', (req, res, next) => {
+  var newUrl = req.url.replace(/^\//, '');
+  request(newUrl, function (error, response, body) {
+    if (error) {
+      return res.status(500).send('Error', error);
+    }
+    res.header('Access-Control-Allow-Origin', '*');
+    //res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    res.send(body)
+    //return res.json({'url': req.url, 'path': req.path, 'u': url})
+  })
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
-
-
-
 
